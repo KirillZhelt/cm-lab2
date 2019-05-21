5e802b3058413b093910d0611830d1a4e4869093
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-05-21 20:46:19 +0300

    Delete unnecessary code

--- cm-lab2/qr.cpp
+++ cm-lab2/qr.cpp
@@ -8,13 +8,6 @@ int Sign(double d) {
 	return (d >= 0) - (d < 0);
 }
 
-void Transpose(double** m, int rows, int columns) {
-	for (int i = 0; i < rows; i++) {
-		for (int j = i + 1; j < columns; j++)
-			swap(m[i][j], m[j][i]);
-	}
-}
-
 void BuildQR(double** m, int rows, int columns, double** qr, double* diag_r) {
 	// copy transpose
 
@@ -47,31 +40,6 @@ void BuildQR(double** m, int rows, int columns, double** qr, double* diag_r) {
 				qr[j][k] -= 2 * scalar_multiply_result * qr[i][k];
 		}
 	}
-}
-
-void SolveQR(double** qr, double* diag_r, int rows, int columns, double* v, double* x) {
-	double* b = new double[rows];
-	CopyVector(v, b, rows);
-
-	for (int i = 0; i < columns - 1; i++) {
-		double scalar_multiply_result = ScalarMultiply(&qr[i][i], &b[i], rows - i);
-
-		for (int j = i; j < rows; j++)
-			b[j] -= 2 * scalar_multiply_result * qr[i][j];
-	}
-
-	// Rx = b'
-	for (int i = columns - 1; i >= 0; i--) {
-		double sum = 0;
-
-		for (int j = i + 1; j < columns; j++)
-			sum += x[j] * qr[j][i];
-
-		x[i] = (b[i] - sum) / diag_r[i];
-	}
-
-
-	delete[] b;
 }
 
 void FindEigenvaluesQR(double** A, int rows, int columns, Complex* eigenvalues) {
--- cm-lab2/qr.h
+++ cm-lab2/qr.h
@@ -7,6 +7,4 @@ struct Complex {
 
 void BuildQR(double** m, int rows, int columns, double** qr, double* diag_r);
 
-void SolveQR(double** qr, double* diag_r, int rows, int columns, double* v, double* x);
-
 void FindEigenvaluesQR(double** A, int rows, int columns, Complex* eigenvalues);
